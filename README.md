d5caac98148965d27a56c107d1660904fc6553d0
MoezKouni <mo.elkouni@gmail.com>
2020-04-02 13:04:09 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,12 @@
+# What is this ?
+
+Get perfect modern buttons for the non-designers
+
+# Installation
+
+`npm i moz-ui --save`
+
+ # Options
+
+ Three types of nice buttons:
+ * *type* - _gradient-blue | _gradient-orange_ (Defaults to soft blue)
---
+++ index.js
@@ -0,0 +1,23 @@
+function Button({type, text}){
+    if(type === 'gradient-blue'){
+        return(
+            <button style={{backgroundImage: "linear-gradient(to right, #fbc2eb 0%, #a6c1ee 51%, #fbc2eb 100%)"}}>
+                {text}
+            </button>
+        )
+    }else if(type === 'gradient-oragne'){
+        return(
+            <button style={{backgroundImage: "linear-gradient(to right, #f6d365 0%, #fda085 51%, #f6d365 100%)"}}>
+                {text}
+            </button>
+        )
+    }else{
+        return(
+            <button style={{backgroundImage: "linear-gradient(to right, #a1c4fd 0%, #c2e9fb 51%, #a1c4fd 100%)"}}>
+                {text}
+            </button>
+        )
+    }
+}
+
+module.exports.Button = Button
