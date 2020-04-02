149470e2cfe199a3b3ad51d91124214c77fa00f0
MoezKouni <mo.elkouni@gmail.com>
2020-04-02 14:57:40 +0100

    666

--- index.js
+++ index.js
@@ -3,15 +3,18 @@ function Button(options){
     if(options.type === 'gradient-blue'){
         btn.className = options.type
         btn.style.backgroundImage = "linear-gradient(to right, #fbc2eb 0%, #a6c1ee 51%, #fbc2eb 100%)"
-        btn.innerHTML = options.text    
+        btn.innerHTML = options.text
+        return btn    
     }else if(options.type === 'gradient-oragne'){
         btn.className = options.type
         btn.style.backgroundImage = "linear-gradient(to right, #f6d365 0%, #fda085 51%, #f6d365 100%)"
         btn.innerHTML = options.text
+        return btn
     }else{
         btn.className = options.type
         btn.style.backgroundImage = "linear-gradient(to right, #a1c4fd 0%, #c2e9fb 51%, #a1c4fd 100%)"
         btn.innerHTML = options.text
+        return btn
     }
 }
 
