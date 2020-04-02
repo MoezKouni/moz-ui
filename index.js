7fd22005064ffb532a8e5b1ebbcdb1cf6ec6c69f
MoezKouni <mo.elkouni@gmail.com>
2020-04-02 14:49:33 +0100

    options

--- index.js
+++ index.js
@@ -1,17 +1,17 @@
-function Button({type, text}){
+function Button(options){
     let btn = document.createElement('button')
-    if(type === 'gradient-blue'){
-        btn.className = type
+    if(options.type === 'gradient-blue'){
+        btn.className = options.type
         btn.style.backgroundImage = "linear-gradient(to right, #fbc2eb 0%, #a6c1ee 51%, #fbc2eb 100%)"
-        btn.innerHTML = text    
-    }else if(type === 'gradient-oragne'){
-        btn.className = type
+        btn.innerHTML = options.text    
+    }else if(options.type === 'gradient-oragne'){
+        btn.className = options.type
         btn.style.backgroundImage = "linear-gradient(to right, #f6d365 0%, #fda085 51%, #f6d365 100%)"
-        btn.innerHTML = text
+        btn.innerHTML = options.text
     }else{
-        btn.className = type
+        btn.className = options.type
         btn.style.backgroundImage = "linear-gradient(to right, #a1c4fd 0%, #c2e9fb 51%, #a1c4fd 100%)"
-        btn.innerHTML = text
+        btn.innerHTML = options.text
     }
 }
 
