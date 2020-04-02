3cac23baa7d21a83ed7d42da4674f5f114dcc012
MoezKouni <mo.elkouni@gmail.com>
2020-04-02 13:41:52 +0100

    third commit

--- index.js
+++ index.js
@@ -1,22 +1,17 @@
 function Button({type, text}){
+    let btn = document.createElement('button')
     if(type === 'gradient-blue'){
-        return(
-            <button style="background-image: linear-gradient(to right, #fbc2eb 0%, #a6c1ee 51%, #fbc2eb 100%)">
-                {text}
-            </button>
-        )
+        btn.className = type
+        btn.style.backgroundImage = "linear-gradient(to right, #fbc2eb 0%, #a6c1ee 51%, #fbc2eb 100%)"
+        btn.innerHTML = text    
     }else if(type === 'gradient-oragne'){
-        return(
-            <button style="background-image: linear-gradient(to right, #f6d365 0%, #fda085 51%, #f6d365 100%)">
-                {text}
-            </button>
-        )
+        btn.className = type
+        btn.style.backgroundImage = "linear-gradient(to right, #f6d365 0%, #fda085 51%, #f6d365 100%)"
+        btn.innerHTML = text
     }else{
-        return(
-            <button style="background-image: linear-gradient(to right, #a1c4fd 0%, #c2e9fb 51%, #a1c4fd 100%)">
-                {text}
-            </button>
-        )
+        btn.className = type
+        btn.style.backgroundImage = "linear-gradient(to right, #a1c4fd 0%, #c2e9fb 51%, #a1c4fd 100%)"
+        btn.innerHTML = text
     }
 }
 
